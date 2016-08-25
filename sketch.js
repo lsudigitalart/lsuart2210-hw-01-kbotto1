a6bb55fdec296d49ee01459cda39c92216a2bbaf
Katrina M Bottomlee <kbotto1@digitalart-1.lsu.edu>
2016-08-25 11:50:39 -0500

    done

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(500, 500, P2D);
+  ellipse(200, 200, 300);
+  ellipse(70, 60, 100);
+  ellipse(140, 60, 70);
+  ellipse(65, 100, 70);
+  ellipse(100, 80, 50);
+  ellipse(330, 60, 100);
+  ellipse(330, 100, 70);
+  ellipse(255, 60, 70);
+  ellipse(290, 80, 50);
+  ellipse(200, 200, 50);
+  ellipse(150, 150, 70);
+  ellipse(250, 150, 70);
+  ellipse(142, 150, 55);
+  ellipse(242, 150, 55);
+  ellipse(180, 275, 40)
+}
